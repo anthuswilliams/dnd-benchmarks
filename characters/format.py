4505a3aa2bb6bb5ce9f3cbe21408e65fba367fd4
tshardey <tierra.duffield@gmail.com>
2023-12-28 00:18:19 +0000

    Remove hit points and proficiencies from class features

--- characters/format.py
+++ characters/format.py
@@ -69,6 +69,8 @@ def get_class_features(input):
     """
     class_feature_list = set()
     for feature in input["classes"][0]["classFeatures"]:
+        if feature["definition"]["name"] in ("Hit Points", "Proficiencies"):
+            continue
         if feature["definition"]["requiredLevel"] <= input["classes"][0]["level"]:
             class_feature_list.add(f"{feature["definition"]["name"]} - {feature["definition"]["description"]}")
     return list(class_feature_list)
@@ -112,8 +114,7 @@ def format_output_file(input):
     """
     # Replace Unicode characters and newlines
     cleaned_input = re.sub(r"\\u201[0-9a-z]{1}", "'", input)
-    cleaned_input = re.sub(r"\\n", " ", cleaned_input)
-    cleaned_input = re.sub(r"\\r", "", cleaned_input)
+    cleaned_input = re.sub(r"\\[nr]", " ", cleaned_input)
 
     # Remove html tags
     soup = BeautifulSoup(cleaned_input, "html.parser")
