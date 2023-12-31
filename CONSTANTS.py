5dce1c999f813148a5a3915b08cb6dfff915c2aa
tshardey <tierra.duffield@gmail.com>
2023-12-31 00:23:21 +0000

    Add constants file

---
+++ CONSTANTS.py
@@ -0,0 +1,25 @@
+DND_CAMPAIGN_THEMES = [
+    "Pirate Adventure",
+    "Political Intrigue",
+    "Epic Quest",
+    "Mystery/Investigation",
+    "Horror/Survival",
+    "Dungeon Delving",
+    "War and Conflict",
+    "Exploration/Discovery",
+    "Heist/Crime",
+    "Monster Hunting",
+]
+
+DND_SETTINGS = [
+    "Forgotten Realms",
+    "Dragonlance",
+    "Greyhawk",
+    "Eberron",
+    "Ravenloft",
+    "Planescape",
+    "Dark Sun",
+    "Spelljammer",
+    "Mystara",
+    "Ravnica",
+]
